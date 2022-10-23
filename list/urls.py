a01bbd7dc3ebb7f317ac497f9e3fcdaf8911aa82
sahad sathar <sahadsathar1997@gmail.com>
2022-10-23 16:46:05 +0500

    delete function generetic view & added detailview

--- list/urls.py
+++ list/urls.py
@@ -1,9 +1,8 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    path('', views.task_view,name='task_view'),
+    path('',views.Task_listview.as_view()),
     path('delete/<int:taskid>/', views.delete,name='delete'),
     path('update/<int:taskid>/', views.update,name='update'),
-    path('tvl/',views.Task_listview.as_view()),
-path('dtl/',views.Task_listview.as_view()),
+    path('dtl/<int:pk>/',views.Detaail_view.as_view()),
 ]
--- list/views.py
+++ list/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from .models import Task
 from .forms import Todoforms
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
 
 class Task_listview(ListView):
@@ -11,15 +12,10 @@ class Task_listview(ListView):
     template_name = 'task_view.html'
     context_object_name = 'obj1'
 
-def task_view(request):
-    obj1=Task.objects.all()
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        priority= request.POST.get('priority')
-        date = request.POST.get('date')
-        obj = Task(name=name, priority=priority, date= date)
-        obj.save()
-    return render(request,'task_view.html', {'obj1':obj1})
+class Detaail_view(DetailView):
+    model = Task
+    template_name = 'detail.html'
+    context_object_name = 'i'
 
 def delete(request, taskid):
     task = Task.objects.get(id=taskid)
