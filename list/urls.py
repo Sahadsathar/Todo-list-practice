097b8e733577dcd7848de8e676b8a97f6c76bbc7
sahad sathar <sahadsathar1997@gmail.com>
2022-10-23 19:34:59 +0500

    added class generic delete view

--- list/urls.py
+++ list/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
 urlpatterns = [
-    path('',views.Task_listview.as_view()),
+    path('',views.Task_listview.as_view(),name= 'index'),
     path('delete/<int:taskid>/', views.delete,name='delete'),
     path('update/<int:taskid>/', views.update,name='update'),
     path('detail/<int:pk>/',views.Detaail_view.as_view(),name= 'detail'),
     path('edit/<int:pk>/',views.Task_update.as_view()),
+    path('cbv_delete/<int:pk>/',views.Task_delete.as_view(), name= 'cbv_delete'),
 ]
--- list/views.py
+++ list/views.py
@@ -5,7 +5,7 @@ from .models import Task
 from .forms import Todoforms
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 # Create your views here.
 
@@ -26,6 +26,12 @@ class Task_update(UpdateView):
     fields = ('name', 'priority', 'date')
     def get_success_url(self):
         return reverse_lazy('detail',kwargs= {'pk':self.object.id})
+
+class Task_delete(DeleteView):
+    model = Task
+    template_name = 'delete.html'
+    success_url = reverse_lazy('index')
+
 
 def delete(request, taskid):
     task = Task.objects.get(id=taskid)
